31562f90b668214f5d85e4c7143d7c6a93966f5c
Alexander Corn <mckay@doctormckay.com>
2015-08-06 19:12:48 -0400

    Initial commit

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
-'use strict';
-let http = require('http');
-let env = process.env;
-let port = env.OPENSHIFT_IOJS_PORT || 1337;
-let ip = env.OPENSHIFT_IOJS_IP || '127.0.0.1';
+const PORT = process.env.OPENSHIFT_IOJS_PORT || 8080;
+const IP = process.env.OPENSHIFT_IOJS_IP || '127.0.0.1';
 
-http.createServer(function(req, res) {
-    let body = `Welcome to io.js on OpenShift!
-  Everything seems to be in order.
-  Running io.js`;
-    for (let v in process.versions) {
-      body += `\n- ${v}: ${process.versions[v]}`;
-    }
+var SteamUser = require('steam-user');
+var Express = require('express');
 
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    res.end(body);
-}).listen(port, ip);
+var app = new Express();
+app.listen(PORT, IP);
+
+app.get('/', function(req, res) {
+	res.redirect("https://github.com/DoctorMcKay/steam-pics-api");
+});
